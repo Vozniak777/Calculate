b5308445a115e66c105c6aa34663e52c9b6b287c
Lev Vozniak <73286766+Vozniak-Hub@users.noreply.github.com>
2021-01-17 14:25:10 +0200

    Initial commit

---
+++ shuf loh.py
@@ -0,0 +1,38 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+print("Нуль завершить роботу калькулятора"
+      "\n ")
+while True:
+    s = input("оберіть знак (+,-,*,/): ")
+    if s == '0':
+        break
+    if s in ('+', '-', '*', '/'):
+        x = float(input("x="))
+        y = float(input("y="))
+        if s == '+':
+            print("%.2f" % (x+y))
+        elif s == '-':
+            print("%.2f" % (x-y))
+        elif s == '*':
+            print("%.2f" % (x*y))
+        elif s == '/':
+            if y != 0:
+                print("%.2f" % (x/y))
+            else:
+                print("ділення на нуль!")
+    else:
+        print("Невірний знак!")
